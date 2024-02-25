bd76e76fee23d82d8166f8a9f4405cb50d95a828
HoSiuFu <cnjpkrtw@gmail.com>
2024-02-25 20:37:21 +0000

    feat: First commit, trying to make something work...

---
+++ ui/modules/apps/enhancedTrafficConfig/app.js
@@ -0,0 +1,27 @@
+angular.module('beamng.apps')
+.constant('Transit_Options', {
+    defaultAggression: 0.3
+})
+.directive('enhancedTrafficConfig', [function() {
+    return {
+        templateUrl: '/ui/modules/apps/enhancedTraficConfig/app.html',
+        replace: true,
+        restrict: 'EA',
+        scope: true,
+        controller: ['$log', '$scope', function ($log, $scope) {
+            $scope.defaultForm = {
+                'baseAggression': Transit_Options.aggression
+            }
+            $scope.formStatus = angular.copy(defaultForm)
+
+            $scope.submit = function (form) {
+                bngApi.engineLua(`extensions.gameplay_traffic.setTrafficVars( ${bngApi.serializeToLua(form)} )`);
+            }
+
+            $scope.reset = function () {
+                $scope.formStatus = angular.copy($scope.defaultForm)
+                bngApi.engineLua(`extensions.gameplay_traffic.setTrafficVars( ${bngApi.serializeToLua($scope.defaultForm)} )`)
+            }
+        }]
+    }
+}])
