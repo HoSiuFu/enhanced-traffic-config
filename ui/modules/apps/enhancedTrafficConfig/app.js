2521ce12badb359425fdd2f98e7b17b06a33e862
HoSiuFu <cnjpkrtw@gmail.com>
2024-02-27 17:15:52 +0000

    Feat: Added two fields to the form, Ai Mode and Ai Awareness. Will be testing it soon enough.

--- ui/modules/apps/enhancedTrafficConfig/app.js
+++ ui/modules/apps/enhancedTrafficConfig/app.js
@@ -1,15 +1,40 @@
 angular.module('beamng.apps')
-.directive('enhancedTrafficConfig', [function() {
+.constant('DEFAULT_TRAFFIC_CONFIG', {
+    baseAggression: 0.3,
+    minRoadDrivability: 0.25,
+    speedLimit: null,
+    aiMode: 'traffic',
+    aiAware: 'auto',
+    enableRandomEvents: false,
+})
+.constant('TRAFFIC_OPTIONS', {
+    aiMode: [
+        { txt: 'Disabled', val: 'disabled'},
+        { txt: 'Traffic', val: 'traffic'},
+        { txt: 'Random', val: 'random'},
+        { txt: 'Span', val:'span' },
+        { txt: 'Manual', val:'manual' },
+        { txt: 'Chase', val:'chase' },
+        { txt: 'Follow', val:'follow' },
+        { txt: 'Flee', val:'flee' },
+        { txt: 'Chase', val:'chase' },
+    ],
+
+    aiAware: [
+        { txt: 'Auto', val:'auto' },
+        { txt: 'Off', val: 'off' },
+        { txt: 'On', val: 'on' },
+    ]
+})
+.directive('enhancedTrafficConfig', ['DEFAULT_TRAFFIC_CONFIG', 'TRAFFIC_OPTIONS',function() {
     return {
         templateUrl: '/ui/modules/apps/enhancedTrafficConfig/app.html',
         replace: true,
         restrict: 'EA',
         scope: true,
         link: function (scope, element, attrs) {
-            scope.defaultForm = {
-                'baseAggression': 0.3
-            }
-            scope.formStatus = angular.copy(scope.defaultForm)
+            scope.formStatus = angular.copy(DEFAULT_TRAFFIC_CONFIG)
+            scope.formOptions = angular.copy(TRAFFIC_OPTIONS)
 
             scope.onSubmit = function (form) {
                 bngApi.engineLua(`extensions.gameplay_traffic.setTrafficVars( ${bngApi.serializeToLua(form)} )`);
