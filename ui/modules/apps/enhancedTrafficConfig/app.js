00560e71ba16b33f00fe0c45a14514329c257b42
HoSiuFu <cnjpkrtw@gmail.com>
2024-02-27 17:33:01 +0000

    Fix: Defining constants like in normal Angular 1.5 is a no go for mods, I went and fixed the issue.

--- ui/modules/apps/enhancedTrafficConfig/app.js
+++ ui/modules/apps/enhancedTrafficConfig/app.js
@@ -1,47 +1,46 @@
 angular.module('beamng.apps')
-.constant('DEFAULT_TRAFFIC_CONFIG', {
-    baseAggression: 0.3,
-    minRoadDrivability: 0.25,
-    speedLimit: null,
-    aiMode: 'traffic',
-    aiAware: 'auto',
-    enableRandomEvents: false,
-})
-.constant('TRAFFIC_OPTIONS', {
-    aiMode: [
-        { txt: 'Disabled', val: 'disabled'},
-        { txt: 'Traffic', val: 'traffic'},
-        { txt: 'Random', val: 'random'},
-        { txt: 'Span', val:'span' },
-        { txt: 'Manual', val:'manual' },
-        { txt: 'Chase', val:'chase' },
-        { txt: 'Follow', val:'follow' },
-        { txt: 'Flee', val:'flee' },
-        { txt: 'Chase', val:'chase' },
-    ],
-
-    aiAware: [
-        { txt: 'Auto', val:'auto' },
-        { txt: 'Off', val: 'off' },
-        { txt: 'On', val: 'on' },
-    ]
-})
-.directive('enhancedTrafficConfig', ['DEFAULT_TRAFFIC_CONFIG', 'TRAFFIC_OPTIONS',function() {
+.directive('enhancedTrafficConfig', [function() {
     return {
         templateUrl: '/ui/modules/apps/enhancedTrafficConfig/app.html',
         replace: true,
         restrict: 'EA',
         scope: true,
         link: function (scope, element, attrs) {
+            const DEFAULT_TRAFFIC_CONFIG = {
+                baseAggression: 0.3,
+                minRoadDrivability: 0.25,
+                speedLimit: null,
+                aiMode: 'traffic',
+                aiAware: 'auto',
+                enableRandomEvents: false,
+            }
             scope.formStatus = angular.copy(DEFAULT_TRAFFIC_CONFIG)
-            scope.formOptions = angular.copy(TRAFFIC_OPTIONS)
+            scope.formOptions = {
+                aiMode: [
+                    { txt: 'Disabled', val: 'disabled'},
+                    { txt: 'Traffic', val: 'traffic'},
+                    { txt: 'Random', val: 'random'},
+                    { txt: 'Span', val:'span' },
+                    { txt: 'Manual', val:'manual' },
+                    { txt: 'Chase', val:'chase' },
+                    { txt: 'Follow', val:'follow' },
+                    { txt: 'Flee', val:'flee' },
+                    { txt: 'Chase', val:'chase' },
+                ],
+            
+                aiAware: [
+                    { txt: 'Auto', val:'auto' },
+                    { txt: 'Off', val: 'off' },
+                    { txt: 'On', val: 'on' },
+                ]
+            }
 
             scope.onSubmit = function (form) {
                 bngApi.engineLua(`extensions.gameplay_traffic.setTrafficVars( ${bngApi.serializeToLua(form)} )`);
             }
 
             scope.onReset = function () {
-                scope.formStatus = angular.copy(scope.defaultForm)
+                scope.formStatus = angular.copy(DEFAULT_TRAFFIC_CONFIG)
                 bngApi.engineLua(`extensions.gameplay_traffic.setTrafficVars( ${bngApi.serializeToLua(scope.defaultForm)} )`)
             }
         }
